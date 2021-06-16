bbb02c5a2ff435f425c78dda18f2f4b9bc9d5ce3
Oscar <os.amilcar@hotmail.com>
2021-06-16 02:33:00 -0600

    information aircraft added to reports

--- src/main/java/org/kodigo/project/Documents/FlightReport.java
+++ src/main/java/org/kodigo/project/Documents/FlightReport.java
@@ -4,7 +4,6 @@ package org.kodigo.project.Documents;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -14,8 +13,8 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.kodigo.project.ApiPublic.WeatherState;
+import org.kodigo.project.models.Aircraft;
 import org.kodigo.project.models.Flight;
-import org.kodigo.project.persistence.AircraftRepository;
 import org.kodigo.project.persistence.IAircraftRepository;
 
 import java.io.FileNotFoundException;
@@ -44,8 +43,9 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
             List<Flight> flightData = new ArrayList<>();
             flightData.add(getFlight().get(0));
 
+
             //Heading of the document
-            String[] header = {"No FLIGHT",	"AIRLINE", "TYPE AIRCRAFT", "SOURCE", "DESTINATION", "DATE","DEPARTURE TIME", "ARRIVAL TIME","WEATHER CONDITION"};
+            String[] header = {"No FLIGHT",	"AIRLINE", "TYPE AIRCRAFT","CAPACITY PASSENGERS","FUEL TANKS" ,"SOURCE", "DESTINATION", "DATE","DEPARTURE TIME", "ARRIVAL TIME","WEATHER CONDITION"};
 
             //Adding style to the excel sheet
             CellStyle style = book.createCellStyle();
@@ -67,6 +67,7 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
 
             for (Flight data: flightData){
                 row = sheet1.createRow(++count);
+                Aircraft aircfraft = aircraftRepository.findByString(data.getTypeAircraft());
 
                 XSSFCell cell = row.createCell(0);
                 cell.setCellValue(data.getNoFlight());
@@ -78,22 +79,31 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
                 cell.setCellValue(data.getTypeAircraft());
 
                 cell = row.createCell(3);
-                cell.setCellValue(data.getSource());
+                cell.setCellValue( aircfraft.getPassengerCapacity());
 
                 cell = row.createCell(4);
-                cell.setCellValue(data.getDestination());
+                cell.setCellValue(aircfraft.getFuelTanksFull());
 
                 cell = row.createCell(5);
-                cell.setCellValue(data.getDate());
+                cell.setCellValue(data.getSource());
 
                 cell = row.createCell(6);
-                cell.setCellValue(data.getDepartureTime());
+                cell.setCellValue(data.getDestination());
 
                 cell = row.createCell(7);
-                cell.setCellValue(data.getArrivalTime());
+                cell.setCellValue(data.getDate());
 
                 cell = row.createCell(8);
+                cell.setCellValue(data.getDepartureTime());
+
+                cell = row.createCell(9);
+                cell.setCellValue(data.getArrivalTime());
+
+                cell = row.createCell(10);
                 cell.setCellValue(weatherState.GetweatherStatus(data.getDestination()));
+
+                cell = row.createCell(11);
+                cell.setCellValue(data.getComments());
             }
 
             try{
@@ -110,11 +120,14 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
 
     @Override
     public String toPdf() {
-        String[] header = {"No FLIGHT",	"AIRLINE", "TYPE AIRCRAFT", "SOURCE", "DESTINATION", "DATE","DEPARTURE TIME", "ARRIVAL TIME","WEATHER CONDITION"};
+        String[] header = {"No FLIGHT",	"AIRLINE", "SOURCE", "DESTINATION", "DATE","DEPARTURE TIME", "ARRIVAL TIME"};
         //Create the document
         Document document = new Document(PageSize.A3.rotate());
         List<Flight> flightData = new ArrayList<>();
         flightData.add(getFlight().get(0));
+        String city = "london";
+        String comments = "";
+        Aircraft aircraft = new Aircraft();
         try {
             FileOutputStream pdfFile = new FileOutputStream("FlightReport.pdf");
 
@@ -133,7 +146,7 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
             document.add(title);
 
             //Creating a table for showing data
-            PdfPTable table = new PdfPTable(8);
+            PdfPTable table = new PdfPTable(7);
             table.setWidthPercentage(100);
             table.getDefaultCell().setUseAscender(true);
             table.getDefaultCell().setUseDescender(true);
@@ -146,19 +159,49 @@ public class FlightReport implements IExportablePdf, IExportableExcel {
 
             //Add the content
             for (Flight data: flightData){
+                city = data.getDestination();
+                comments = data.getComments();
+                aircraft = aircraftRepository.findByString(data.getTypeAircraft());
                 table.addCell(Integer.toString(data.getNoFlight()));
                 table.addCell(data.getAirline());
-                table.addCell(data.getTypeAircraft());
                 table.addCell(data.getSource());
                 table.addCell(data.getDestination());
                 table.addCell(data.getDate());
                 table.addCell(data.getDepartureTime());
                 table.addCell(data.getArrivalTime());
-                table.addCell(weatherState.GetweatherStatus(data.getDestination()));
             }
 
             //Add the table to the document
             document.add(table);
+
+            StringBuilder aircraftInfo = new StringBuilder();
+            Paragraph subtitleWeather = new Paragraph(String.format("\n\n%s\n", "WEATHER CONDITION"), FontFactory.getFont("arial",12,BaseColor.DARK_GRAY));
+            document.add(subtitleWeather);
+
+            Paragraph weatherInfo = new Paragraph(String.format("%s\n", weatherState.GetweatherStatus(city)), FontFactory.getFont("arial",12,BaseColor.DARK_GRAY));
+            document.add(weatherInfo);
+
+            Paragraph aircraftSubtitle = new Paragraph(String.format("\n\n%s\n", "AIRCRAFT INFO"), FontFactory.getFont("arial",10,BaseColor.DARK_GRAY));
+            document.add(aircraftSubtitle);
+
+            aircraftInfo.append("Type aircraft: ")
+                    .append(aircraft.getModel())
+                    .append("\n")
+                    .append("CAPACITY PASSENGERS: ")
+                    .append(aircraft.getPassengerCapacity())
+                    .append("\n")
+                    .append("FUEL TANKS: ")
+                    .append(aircraft.getFuelTanksFull());
+
+            Paragraph aircraftResult = new Paragraph(String.format("%s\n",aircraftInfo), FontFactory.getFont("arial",10,BaseColor.DARK_GRAY));
+            document.add(aircraftResult);
+
+            Paragraph commentsSubtitle = new Paragraph(String.format("\n\n%s\n", "COMMENTS"), FontFactory.getFont("arial",10,BaseColor.DARK_GRAY));
+            document.add(commentsSubtitle);
+
+            Paragraph infoComments = new Paragraph(String.format("\n%s",comments), FontFactory.getFont("arial",10,BaseColor.DARK_GRAY));
+            document.add(infoComments);
+
 
             //Close document
             document.close();
